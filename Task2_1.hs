0f2140dae264e63d115c1625f7676cc5db0cffa7
Vadim Dyachkov <vadik.dyachkov@gmail.com>
2019-11-01 10:13:07 +0300

    implement treemap

--- Task2_1.hs
+++ Task2_1.hs
@@ -7,30 +7,55 @@ module Task2_1 where
 -}
 
 import Todo(todo)
+import Prelude hiding (lookup)
 
 -- Ассоциативный массив на основе бинарного дерева поиска
 -- Ключи - Integer, значения - произвольного типа
-data TreeMap v = ChangeMe
+data TreeMap v =
+    EmptyTree
+  | Node Integer v (TreeMap v) (TreeMap v)
+  deriving Show
 
 -- Пустое дерево
 emptyTree :: TreeMap v
-emptyTree = todo
+emptyTree = EmptyTree
 
 -- Содержится ли заданный ключ в дереве?
-contains :: TreeMap v -> Integer -> Bool
-contains t k = todo
+contains :: Integer -> TreeMap v -> Bool
+contains _ EmptyTree = False
+contains x (Node k v t1 t2)
+  | x == k    = True
+  | x < k     = contains x t1
+  | otherwise = contains x t2
 
 -- Значение для заданного ключа
-lookup :: Integer -> TreeMap v -> v
-lookup k t = todo
+lookup :: Integer -> TreeMap v -> Maybe v
+lookup _ EmptyTree = Nothing
+lookup x (Node k v t1 t2)
+  | x == k    = Just v
+  | x < k     = lookup x t1
+  | otherwise = lookup x t2
 
 -- Вставка пары (ключ, значение) в дерево
 insert :: (Integer, v) -> TreeMap v -> TreeMap v
-insert (k, v) t = todo
+insert (x, y) EmptyTree = Node x y EmptyTree EmptyTree
+insert xy@(x, y) (Node k v t1 t2)
+  | x == k    = Node x y t1 t2
+  | x < k     = Node k v (insert xy t1) t2
+  | otherwise = Node k v t1 (insert xy t2)
 
 -- Удаление элемента по ключу
 remove :: Integer -> TreeMap v -> TreeMap v
-remove i t = todo
+remove _ EmptyTree = EmptyTree
+remove x (Node k v t1 t2)
+  | x == k    = merge t1 t2
+  | x < k     = Node k v (remove x t1) t2
+  | otherwise = Node k v t1 (remove x t2)
+
+merge :: TreeMap v -> TreeMap v -> TreeMap v
+merge EmptyTree t2 = t2
+merge t1 EmptyTree = t1
+merge (Node k v t11 t12) t2 = Node k v t11 (merge t12 t2)
 
 -- Поиск ближайшего снизу ключа относительно заданного
 nearestLE :: Integer -> TreeMap v -> (Integer, v)
@@ -38,12 +63,22 @@ nearestLE i t = todo
 
 -- Построение дерева из списка пар
 treeFromList :: [(Integer, v)] -> TreeMap v
-treeFromList lst = todo
+treeFromList kvs = foldr insert EmptyTree kvs
 
 -- Построение списка пар из дерева
 listFromTree :: TreeMap v -> [(Integer, v)]
-listFromTree t = todo
+listFromTree EmptyTree = []
+listFromTree (Node k v t1 t2) = concat [listFromTree t1, [(k, v)], listFromTree t2]
 
 -- Поиск k-той порядковой статистики дерева
 kMean :: Integer -> TreeMap v -> (Integer, v)
 kMean i t = todo
+
+(|++|) :: TreeMap v -> (Integer, v) -> TreeMap v
+(|++|) = flip insert
+
+(|--|) :: TreeMap v -> Integer -> TreeMap v
+(|--|) = flip remove
+
+-- Test
+x = emptyTree |++| (2, "2") |++| (1, "1") |++| (3, "3") |--| 2 |++| (5, "5") |++| (0, "0")
