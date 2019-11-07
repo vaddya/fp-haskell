492d1c46d8a5ad4f8ca349a2545ca257334edd17
vaddya <vadim.dyachkov@corp.mail.ru>
2019-11-07 16:22:50 +0300

    add test for task2

--- Task2_2.hs
+++ Task2_2.hs
@@ -67,7 +67,7 @@ elem y lst = foldl f False lst
 -- Список чисел в диапазоне [from, to) с шагом step
 rangeTo :: Integer -> Integer -> Integer -> [Integer]
 rangeTo from to step = unfoldr f from
-  where f x = if x < to then Just (x, x + step) else Nothing
+  where f x = if (x < to && step > 0) || (x > to && step < 0) then Just (x, x + step) else Nothing
 
 -- Конкатенация двух списков
 append :: [a] -> [a] -> [a]
@@ -80,3 +80,25 @@ groups lst n = unfoldr f lst
   where
     f list = if null list then Nothing else Just (take n' list, drop n' list)
     n' = fromIntegral n
+
+-- Test
+testSum lst exptected = exptected == sum lst
+testSums = [testSum [1,2,3] 6, testSum [] 0, testSum [-1,-2,-3,-4] (-10)]
+
+testProduct lst expected = expected == product lst
+testProducts = [testProduct [1,2,3] 6, testProduct [] 1, testProduct [0,1,2] 0]
+
+testFilterPositive lst expected = expected == filterNot (\x -> x <= 0) lst
+testFilterPositives = [testFilterPositive [-1,2,3] [2,3], testFilterPositive [-2,-3] [], testFilterPositive [1,2] [1,2]]
+
+testReverse lst = (reverse . reverse) lst == lst
+testReverses = [testReverse [1,2,3,4], testReverse [1]]
+
+testRange f t s expected = expected == rangeTo f t s
+testRanges = [testRange 1 10 3 [1,4,7], testRange (-5) 6 5 [-5,0,5], testRange 10 1 1 [], testRange 10 7 (-2) [10,8]]
+
+testGroup lst n exptected = exptected == groups lst n
+testGroups = [testGroup [1,2,3,4] 3 [[1,2,3],[4]], testGroup [1,2,3] 4 [[1,2,3]], testGroup [1,2,3] 1 [[1],[2],[3]]]
+
+testAll = map (\b -> if b then "OK" else "FAIL") $
+  Prelude.concatMap id [testSums, testProducts, testFilterPositives, testReverses, testRanges, testGroups]
